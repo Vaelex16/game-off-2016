6b841de2e0bb5335b4696f78c09b34a8ddda5125
Vaelex16 <sean.szuma1@gmail.com>
2016-11-07 21:59:58 -0600

    Castle courtyard Models introduced. Restructured folders updated sword interaction to destroy objects.

--- Link To The Past/Assets/Scripts/WeaponController.cs
+++ Link To The Past/Assets/Scripts/WeaponController.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WeaponController : MonoBehaviour {
 
-
+    public List<GameObject> interactables;
     public GameObject sword;
     public GameObject shield;
     private float swingDelayMax = 0.75f;
     private float swingDelayCurrent;
     private bool canSwing = true;
+    
 
     // Use this for initialization
     void Start () 
@@ -40,6 +42,15 @@ public class WeaponController : MonoBehaviour {
         swingDelayCurrent = swingDelayMax;
         sword.GetComponent<Animator>().SetBool("Swing", true);
         Invoke("StopSwing", 0.03f);
+        if(interactables.Count > 0)
+            Invoke("HitInteractables", .015f);
+    }
+
+    void HitInteractables()
+    {
+        foreach (GameObject obj in interactables)
+            obj.GetComponent<DestructableObject>().DestroyObject();
+        interactables.Clear();
     }
 
     void StopSwing()
